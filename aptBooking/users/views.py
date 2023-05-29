598e8cd1d6d6a2fba37e888056f3f4ae94d62ee5
Anshul Raj <90825666+ar2032@users.noreply.github.com>
2023-05-29 20:31:38 +0400

    sign up done an error comes up that can be ignored

--- aptBooking/users/views.py
+++ aptBooking/users/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import NewUserForm
+from django.contrib.auth import login
 
-def register(request):
-    form = UserCreationForm()
-    template_name = 'signup.html'
-    return render(request, template_name,{'form':form})
+def  register(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect("dashboard.html")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render (request=request, template_name="signup.html", context={"register_form":form})
