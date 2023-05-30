5700447a8645a9082862ea6aa32450c244efc66c
Gauri Revankar <90549257+GauriR06@users.noreply.github.com>
2023-05-30 16:57:48 +0400

    Update urls.py

--- aptBooking/users/urls.py
+++ aptBooking/users/urls.py
@@ -1,9 +1,6 @@
+from django.conf.urls import include
 from django.urls import path
-from . import views 
-
-#URLConf
+from . import views
 urlpatterns = [
-    path("add/", views.add, name='add'),
-    path("hello/", views.say_hello, name='hello'),
-    
+    path('register/', views.register, name='register'),
 ]
