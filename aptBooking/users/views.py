9dc6eb972562107112ac36ccffce9adac782267f
Gauri Revankar <90549257+GauriR06@users.noreply.github.com>
2023-05-30 16:58:52 +0400

    Update views.py

--- aptBooking/users/views.py
+++ aptBooking/users/views.py
@@ -1,28 +1,16 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-#-----
-from django.http import HttpResponse
-#-----
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import NewUserForm
+from django.contrib.auth import login
 
-# def register(request):
-#     form = UserCreationForm()
-#     template_name = 'signup.html'
-#     return render(request, template_name,{'form':form})
-
-def register(request):
-    context ={}
-    context['form']= UserCreationForm()
-    return render(request, "users/register.html", context)
-
-#-----
-#request handler
-def say_hello(request):
-    return render(request,'users/hello.html', {'name' : 'Sally'})
-
-def add(request):
-    num1 = int (request.GET['num1'])
-    num2 = int (request.GET['num2'])
-    res = num1 + num2
-    return render(request,'users/result.html', {'result': res})
-
-#-----
+def  register(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect("dashboard.html")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render (request=request, template_name="signup.html", context={"register_form":form})
