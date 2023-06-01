61ee11b6c07984c3344a8569ab09e7fb6a00614a
vitvr <victor.verde.vv@outlook.com>
2023-06-01 20:36:10 +0400

    appointments now show actual info instead of IDs

--- aptBooking/users/urls.py
+++ aptBooking/users/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include
 from django.urls import path
 from . import views
+from .views import appointmentsListView
+
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('appointments/', views.appointments, name = "appointments")
+    path('appointments/', appointmentsListView.as_view())
 ]
