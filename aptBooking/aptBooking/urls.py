5def3f10d6afe4f898ea69eb16219d8720ad86cc
Gauri Revankar <90549257+GauriR06@users.noreply.github.com>
2023-06-02 11:58:43 +0400

    Update urls.py

--- aptBooking/aptBooking/urls.py
+++ aptBooking/aptBooking/urls.py
@@ -23,12 +23,10 @@ from users import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    #path('register/',user_views.register, name='register'),
+    path('register/',user_views.register, name='register'),
     path('users/', include('django.contrib.auth.urls')),
     path('users/',include('users.urls')),
     path('', TemplateView.as_view(template_name='dashboard.html'), name='home'),
     path('login/', LoginView.as_view(), name='login'),
-    path('appointment', views.appointment_api),
-    #path('appointments', views.update_appointment_status),
-    # path('agents', views.agent_info)
+    path('appointment', views.appointment_api)
 ]
