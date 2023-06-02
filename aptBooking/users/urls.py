2844e3c47a1a56c2d225b2e8c12e209ea009d3ff
Gauri Revankar <90549257+GauriR06@users.noreply.github.com>
2023-06-02 11:48:56 +0400

    Update urls.py

--- aptBooking/users/urls.py
+++ aptBooking/users/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import include
 from django.urls import path
-from .views import appointmentsListView
 from . import views
+from .views import appointmentsListView
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    #path('appointments/', views.update_appointment_status, name = "appointments"),
-    path('update_appointment/', views.update_appointment_status, name='update_appointment_status'),
-    # path('agents/', views.agent_api, name = "agents")
+    # path('appointments/', appointmentsListView.as_view())
+    path('update_appointment/', views.update_appointment_status, name='update_appointment_status')
 ]
-
