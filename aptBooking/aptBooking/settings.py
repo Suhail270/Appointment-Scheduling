56c11ccf80c0d54364cc5f0abd1a18ab91d44b64
Archie138 <kar.archisa@gmail.com>
2023-06-04 12:32:39 +0400

    little bit of css added

--- aptBooking/aptBooking/settings.py
+++ aptBooking/aptBooking/settings.py
@@ -130,7 +130,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/"),
+)
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/users/login/'
 
--- aptBooking/sales/forms.py
+++ aptBooking/sales/forms.py
@@ -4,10 +4,10 @@ from .models import TimeChoices, Customer
 # Create your forms here.
 
 class NewCustForm(forms.Form):
-    first_name = forms.CharField(max_length = 200)
-    last_name = forms.CharField(max_length = 200)
-    email_address = forms.EmailField(max_length = 200)
-    mobile_number = forms.CharField()
+    first_name = forms.CharField(max_length = 200, widget=forms.TextInput(attrs={'class' : 'custinput'}))
+    last_name = forms.CharField(max_length = 200, widget=forms.TextInput(attrs={'class' : 'custinput'}))
+    email_address = forms.EmailField(max_length = 200, widget=forms.TextInput(attrs={'class' : 'custinput'}))
+    mobile_number = forms.CharField(widget=forms.TextInput(attrs={'class' : 'custinput'}))
 
     # class Meta:
     #     model = Customer
